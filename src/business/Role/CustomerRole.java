e3211445eb74aef954feef1355c0b98385b22cd4
Sangram Shinde <shinde.san@northeastern.edu>
2023-12-10 15:05:33 -0500

    Customer Role Added

---
+++ src/business/Role/CustomerRole.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package business.Role;
+
+import business.MainSystem;
+import business.Enterprise.Enterprise;
+import business.Organization.Organization;
+import business.Organization.CustomerOrganization;
+import business.UserAccount.UserAccount;
+import ui.CustomerOrganization.DashboardCustomer;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author vidis
+ */
+public class CustomerRole extends Role{
+    //overriding main panel with volunteer work area whenever volunteer role is called
+        @Override
+        public JPanel createWorkArea(JPanel userProcessContainer,UserAccount account,Organization organization,Enterprise enterprise,MainSystem system)
+        {
+            return new DashboardCustomer(userProcessContainer,account,(CustomerOrganization)organization,enterprise,system);
+        }   
+        @Override
+        public String toString()
+{
+   return Role.RoleType.CustomerRole.getValue();
+}
+}
