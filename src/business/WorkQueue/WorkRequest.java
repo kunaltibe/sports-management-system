548847a01f8ef3f7e70c118451b836c245bf4142
Sangram Shinde <shinde.san@northeastern.edu>
2023-12-10 14:35:42 -0500

    Created Work Request Queue

---
+++ src/business/WorkQueue/WorkRequest.java
@@ -0,0 +1,163 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package business.WorkQueue;
+
+import business.EventMaker.EventMaker;
+import business.Selector.Selector;
+import business.Scheduler.Scheduler;
+import business.Coach.Coach;
+import business.Provider.Item;
+import business.Provider.Provider;
+import business.UserAccount.UserAccount;
+import business.Player.Player;
+import business.Customer.Customer;
+import java.util.Date;
+
+/**
+ *
+ * @author prath
+ */
+public abstract class WorkRequest {
+    private String subject;
+    private UserAccount sender;
+    private UserAccount reciever;
+    private String status;
+    private Date requestDate;
+    private Date resolveDate;
+    private EventMaker eventMaker;
+    private Player changeSeeker;
+    private Scheduler ngo;
+    private Provider provider;
+    private Item item;
+    private Customer volunteer;
+    private Coach police;
+    private Selector fireMan;
+
+    public Selector getFireMan() {
+        return fireMan;
+    }
+
+    public void setFireMan(Selector fireman) {
+        this.fireMan = fireman;
+    }
+    
+    public WorkRequest(){
+        requestDate = new Date();
+    }
+    
+    public Coach getPolice() {
+        return police;
+    }
+
+    public void setPolice(Coach police) {
+        this.police = police;
+    }
+
+    public Customer getVolunteer() {
+        return volunteer;
+    }
+
+    public void setVolunteer(Customer volunteer) {
+        this.volunteer = volunteer;
+    }
+
+    
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
+    
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public UserAccount getSender() {
+        return sender;
+    }
+
+    public void setSender(UserAccount sender) {
+        this.sender = sender;
+    }
+
+    public UserAccount getReciever() {
+        return reciever;
+    }
+
+    public void setReciever(UserAccount reciever) {
+        this.reciever = reciever;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public Provider getProvider() {
+        return provider;
+    }
+
+    public void setProvider(Provider provider) {
+        this.provider = provider;
+    }
+
+    
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Date getRequestDate() {
+        return requestDate;
+    }
+
+    public void setRequestDate(Date requestDate) {
+        this.requestDate = requestDate;
+    }
+
+    public Date getResolveDate() {
+        return resolveDate;
+    }
+
+    public void setResolveDate(Date resolveDate) {
+        this.resolveDate = resolveDate;
+    }
+
+    public EventMaker getEventMaker() {
+        return eventMaker;
+    }
+
+    public void setEventMaker(EventMaker eventMaker) {
+        this.eventMaker = eventMaker;
+    }
+
+    public Player getChangeSeeker() {
+        return changeSeeker;
+    }
+
+    public void setChangeSeeker(Player changeSeeker) {
+        this.changeSeeker = changeSeeker;
+    }
+
+    public Scheduler getNgo() {
+        return ngo;
+    }
+
+    public void setNgo(Scheduler ngo) {
+        this.ngo = ngo;
+    }
+
+    @Override
+    public String toString(){
+        return status;
+    }
+}
+
---
+++ src/business/WorkQueue/WorkRequestQueue.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package business.WorkQueue;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author prath
+ */
+public class WorkRequestQueue {
+    
+    private ArrayList<WorkRequest> workRequestList;
+    
+    public WorkRequestQueue(){
+        workRequestList=new ArrayList<>();
+    }
+
+    public ArrayList<WorkRequest> getWorkRequestList() {
+        return workRequestList;
+    }
+    
+    
+}
